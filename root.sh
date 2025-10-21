bda637c8c5ef3b3ad46ec2824e372822e4a86f35
lavabyte <138317871+lavabyte@users.noreply.github.com>
2025-10-21 22:05:31 +0200

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+ROOTFS_DIR=$(pwd)
+export PATH=$PATH:~/.local/usr/bin
+max_retries=50
+timeout=1
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH_ALT=amd64
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH_ALT=arm64
+else
+  printf "Unsupported CPU architecture: ${ARCH}"
+  exit 1
+fi
+wget --tries=$max_retries --timeout=$timeout --no-hsts -O rootfs.tar.gz "https://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.1-base-${ARCH_ALT}.tar.gz"
+tar -xf rootfs.tar.gz -C $ROOTFS_DIR
+rm rootfs.tar.gz
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  mkdir $ROOTFS_DIR/usr/local/bin -p
+  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/lavabyte/root/main/proot-${ARCH}"
+  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
+    rm $ROOTFS_DIR/usr/local/bin/proot -rf
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/lavabyte/root/main/proot-${ARCH}"
+    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
+      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+      break
+    fi
+    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    sleep 1
+  done
+  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+fi
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
+  touch $ROOTFS_DIR/.installed
+fi
+clear
+$ROOTFS_DIR/usr/local/bin/proot \
+  --rootfs="${ROOTFS_DIR}" \
+  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit su
