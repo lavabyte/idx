080631f9a07bb324c5dc88da07286b99b3b8afef
lavabyte <138317871+lavabyte@users.noreply.github.com>
2025-10-21 23:46:09 +0200

    Clone root repo and execute root.sh
    Clone the repository if it doesn't exist and run the script.

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 if [ ! -d "./root" ]; then
     git clone https://github.com/lavabyte/root
+    printf "exit" | bash root/root.sh
+    mv vm.sh root/root/
 fi
 printf "bash vm.sh" | bash root/root.sh
