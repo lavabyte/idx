2196f1f8d98e18aaf11a303a4cf53699c9b3f910
lavabyte <138317871+lavabyte@users.noreply.github.com>
2025-10-21 22:45:17 +0200

    Fix path for moving vm.sh to root directory

--- run.sh
+++ run.sh
@@ -35,8 +35,8 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   touch $ROOTFS_DIR/.installed
 fi
 clear
-if [[ -f "./vm.sh" ]]; then
-    mv ./vm.sh ./
+if [[ -f "vm.sh" ]]; then
+    mv vm.sh root/
 fi
 $ROOTFS_DIR/usr/local/bin/proot \
   --rootfs="${ROOTFS_DIR}" \
