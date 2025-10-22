ce4a33512f27878adfa28cc8951fa2831d253fa4
lavabyte <138317871+lavabyte@users.noreply.github.com>
2025-10-22 19:50:53 +0200

    Simplify run.sh to directly execute root.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-cd root >/dev/null 2>/dev/null
-if [ ! -d "./root" ]; then
-    git clone https://github.com/lavabyte/root >/dev/null 2>/dev/null
-    cd root
-    printf "exit" | bash root.sh >/dev/null 2>/dev/null
-    mv ../vm.sh root/
-fi
-printf "bash vm.sh" | bash root.sh
+bash root.sh
