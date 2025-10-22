36983dfe5c733670d97ccd39ad2b6201731f980e
lavabyte <138317871+lavabyte@users.noreply.github.com>
2025-10-22 20:26:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ ! -d "./root" ]; then
     git clone https://github.com/lavabyte/root >/dev/null 2>/dev/null
     cd root
-    printf "echo -e '\nbash vm.sh' >> .bashrc && exit" | bash root.sh >/dev/null 2>/dev/null
+    printf "echo -e '\nbash vm.sh' >> .bashrc && echo -e '\nexit' >> .bashrc && exit" | bash root.sh >/dev/null 2>/dev/null
     mv ../vm.sh root/
 fi
 bash root.sh
